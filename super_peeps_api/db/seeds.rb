6b3bd309c25f4500f691679d61269e426b329045
rhaynes3k <rhaynes3k@gmail.com>
2021-08-29 18:19:19 -0500

    api folder commit

---
+++ super_peeps_api/db/seeds.rb
@@ -0,0 +1,15 @@
+# This file should contain all the record creation needed to seed the database with its default values.
+# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
+#
+# Examples:
+#
+#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
+#   Character.create(name: 'Luke', movie: movies.first)
+
+# BASE_URL = 'https://superheroapi.com/api.php/10225229547146375'
+#
+# Index_a = BASE_URL + '/search/a'
+
+# response = RestClient.get('https://superheroapi.com/api.php/10225229547146375/search/a')
+# charsArray = JSON.parse(response)
+# binding.pry
