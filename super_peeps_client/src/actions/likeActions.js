4da94f97a424fe07cb90ded564e0fb6cf9145a75
rhaynes3k <rhaynes3k@gmail.com>
2021-09-12 20:32:59 -0500

    like button fetch issue resolved

--- super_peeps_client/src/actions/likeActions.js
+++ super_peeps_client/src/actions/likeActions.js
@@ -1,22 +1,23 @@
 
 export const likeCharacters = (likes, id) => {
   console.log('LIKES_FROM_ADD', likes)
+  let chr_updt = {likes: likes.likes}
+  console.log(chr_updt)
   return (dispatch) => {
     fetch(`http://localhost:3000/characters/${likes.id}`,
       {
-        method: 'PUT',
+        method: 'PATCH',
         headers: {
           'Content-Type': 'application/json',
         Accept: 'application/json'
           },
-        body: JSON.stringify({
-          likes: likes.likes
-        })
+        body: JSON.stringify(chr_updt)
       })
         .then(response => response.json())
-        .then(data => console.log('Liked', data))
+        .then(data => console.log('DATA', data))
         .catch((error) => {
         console.log('Error:', error);
         })
+
   }
 }
